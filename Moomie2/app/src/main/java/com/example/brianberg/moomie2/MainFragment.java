2b3e87998f48c72c618925697929a6d79cbe0327
bsberg <bsberg@mtu.edu>
2016-02-22 13:29:55 -0500

    Initial Dump

---
+++ Moomie2/app/src/main/java/com/example/brianberg/moomie2/MainFragment.java
@@ -0,0 +1,152 @@
+package com.example.brianberg.moomie2;
+
+import android.content.Intent;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainFragment extends Fragment {
+
+    private TextView mTextDetails;
+    private CallbackManager mCallbackManager;
+
+    private AccessTokenTracker mTokenTracker;
+    private ProfileTracker mProfileTracker;
+
+    private FacebookCallback<LoginResult> mCallback= new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            AccessToken accessToken = loginResult.getAccessToken();
+            mTokenTracker= new AccessTokenTracker() {
+                @Override
+                protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                    
+                }
+            };
+            mTokenTracker.startTracking();
+            
+            mProfileTracker = new ProfileTracker() {
+                @Override
+                protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                            
+                }
+            };
+            mProfileTracker.startTracking();
+            Profile profile = Profile.getCurrentProfile();
+            displayWelcomeMessage(profile);
+        }
+
+        @Override
+        public void onCancel() {
+
+        }
+
+        @Override
+        public void onError(FacebookException error) {
+
+        }
+    };
+
+    public MainFragment() {
+
+        
+    }
+
+    public MainFragment(AccessTokenTracker mTokenTracker) {
+
+
+    }
+
+    public void onCreate(Bundle SavedInstanceState){
+        super.onCreate(SavedInstanceState);
+        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+        mCallbackManager = CallbackManager.Factory.create();
+
+        AccessTokenTracker tracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldToken, AccessToken newToken) {
+
+            }
+        };
+
+        ProfileTracker profileTracker = new ProfileTracker(){
+
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile newProfile) {
+
+            }
+        };
+
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    public void displayWelcomeMessage(Profile profile){
+        if(profile != null){
+            mTextDetails.setText("Welcome" + profile.getName());
+        }
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+        LoginButton loginButton = (LoginButton) view.findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
+        loginButton.setFragment(this);
+        loginButton.registerCallback(mCallbackManager, mCallback);
+
+        mTextDetails = (TextView) view.findViewById(R.id.textView);
+
+    }
+
+    public void onResume() {
+        super.onResume();
+        Profile profile =Profile.getCurrentProfile();
+        displayWelcomeMessage(profile);
+    }
+
+    public void onStop() {
+        super.onStop();
+        mTokenTracker.stopTracking();
+        mProfileTracker.stopTracking();
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void setmTokenTracker(AccessTokenTracker mTokenTracker) {
+        this.mTokenTracker = mTokenTracker;
+    }
+
+    public void setmProfileTracker(ProfileTracker mProfileTracker) {
+        this.mProfileTracker = mProfileTracker;
+    }
+}
