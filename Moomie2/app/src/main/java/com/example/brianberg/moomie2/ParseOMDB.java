47ab497262598255a85e4c48bc5bbbec102072d0
Jermasaurus <jerry.sommerfeld@gmail.com>
2016-04-01 13:56:00 -0400

    Started work on XML parser
    -need to add a network Activity to retrieve results from the OMDB API
    -need a new way to parse the ID, maybe a string?
    -add more data to parse for full movie details

---
+++ Moomie2/app/src/main/java/com/example/brianberg/moomie2/ParseOMDB.java
@@ -0,0 +1,139 @@
+package com.example.brianberg.moomie2;
+
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Jerry Sommerfeld on 4/1/2016.
+ */
+public class ParseOMDB {
+
+    private static final String ns = null;
+
+    public List parse(InputStream in) throws XmlPullParserException, IOException {
+        try {
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(in, null);
+            parser.nextTag();
+            return  readFeed(parser);
+        } finally {
+            in.close();
+        }
+    }
+
+    private List readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
+        List entries = new ArrayList();
+
+        parser.require(XmlPullParser.START_TAG, ns, "feed");
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if(parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+
+            // Starts by looking at the entry tag
+            if(name.equals("entry")) {
+                entries.add(readEntry(parser));
+            } else {
+                skip(parser);
+            }
+        }
+        return entries;
+    }
+
+    public static class Entry {
+        public final int id;
+        public final String title;
+        public final String plot;
+
+        private Entry(int id, String title, String plot) {
+            this.id = id;
+            this.title = title;
+            this.plot = plot;
+        }
+    }
+
+    private Entry readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "entry");
+        int id = 0;
+        String title = null;
+        String plot = null;
+
+        while(parser.next() != XmlPullParser.END_TAG) {
+            if(parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if(name.equals("title")) {
+                title = readTitle(parser);
+            } else if(name.equals("plot")) {
+                plot = readPlot(parser);
+            } else if(name.equals("id")) {
+                id = readID(parser);
+            } else {
+                skip(parser);
+            }
+        }
+        return new Entry(id, title, plot);
+    }
+
+    // Extracts the title for the movie
+    private String readTitle(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "title");
+        String title = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "title");
+        return title;
+    }
+
+    // Extracts the plot for the movie
+    private String readPlot(XmlPullParser parser) throws  IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "plot");
+        String plot = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "plot");
+        return plot;
+
+    }
+
+    // Extracts the text values for the tags title and plot
+    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String result = "";
+        if(parser.next() == XmlPullParser.TEXT) {
+            result = parser.getText();
+            parser.nextTag();
+        }
+        return result;
+    }
+
+    // Extracts the ID for the movie
+    private int readID(XmlPullParser parser) throws IOException, XmlPullParserException {
+        return 1;
+    }
+
+
+    // Skip what you don't want
+    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if(parser.getEventType() != XmlPullParser.START_TAG) {
+            throw new IllegalStateException();
+        }
+        int depth = 1;
+        while (depth != 0) {
+            switch (parser.next()) {
+                case XmlPullParser.END_TAG:
+                    depth--;
+                    break;
+                case XmlPullParser.START_TAG:
+                    depth++;
+                    break;
+            }
+        }
+    }
+
+}
