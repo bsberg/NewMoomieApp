abbfd790e66c90000f8d813171a29b9b6790c4c3
rkula <robert.kula@marquette.edu>
2016-04-12 21:39:57 -0400

    Better Scrollable Movie Version
    Scrolls smoothly

--- Moomie2/app/src/main/java/com/example/brianberg/moomie2/MovieAdapter.java
+++ Moomie2/app/src/main/java/com/example/brianberg/moomie2/MovieAdapter.java
@@ -41,22 +41,6 @@ public class MovieAdapter extends ArrayAdapter<MovieObject> {
         TextView rating;
     }
 
-    /*@Override
-    public void add(Object object) {
-        super.add(object);
-        list.add(object);
-    }*/
-
-    /*@Override
-    public int getCount() {
-        return this.list.size();
-    }*/
-
-    /*@Override
-    public Object getItem(int position) {
-        return this.list.get(position);
-    }*/
-
     @Override
     public View getView(int position, View convertView, ViewGroup Parent) {
         MovieObject movieObject = getItem(position);
@@ -72,117 +56,7 @@ public class MovieAdapter extends ArrayAdapter<MovieObject> {
         movieRating.setText(movieObject._rating);
         movieTitle.setText(movieObject._title);
 
-        setMoviePoster(moviePoster, movieObject._posterURL);
-        //Bitmap bitmap = getMoviePosterImageFromUrl(movieObject.getPosterURL());
-
-        moviePoster.setImageBitmap(bitmap);
+        moviePoster.setImageBitmap(movieObject.get_posterBitMap());
         return convertView;
-        /*View row;
-        DataHandler handler;
-        row = convertView;
-        if(convertView == null)
-        {
-            LayoutInflater inflater = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            row = inflater.inflate(R.layout.movie_row_layout, Parent, false);
-            handler = new DataHandler();
-            handler.Poster = (ImageView) row.findViewById(R.id.movie_poster);
-            handler.title = (TextView) row.findViewById(R.id.movie_title);
-            handler.rating = (TextView) row.findViewById(R.id.movie_rating);
-
-            row.setTag(handler);
-        }
-        else
-        {
-            handler = (DataHandler) row.getTag();
-        }
-
-
-        MovieObject dataProvider;
-        dataProvider = (MovieObject) this.getItem(position);
-        handler.Poster.setImageDrawable(getMoviePosterImageFromUrl(dataProvider.getPosterURL()));
-        handler.rating.setText(dataProvider.getMoomieRating());
-        handler.title.setText(dataProvider.getTitle());
-        //LayoutInflater inflater = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        //row = inflater.inflate(R.layout.movie_row_layout, Parent, false);
-        return row;*/
-    }
-
-    void setMoviePoster(ImageView moviePoster, final String url){
-        new Thread() {
-            public void run() {
-                InputStream in = null;
-
-                //msg = Message.obtain();
-                //msg.what = 1;
-
-                try {
-                    Log.d("ToolsFragment: ", url);
-                    in = openHttpConnection(url);
-                    bitmap = BitmapFactory.decodeStream(in);
-
-                    // ImageView ImageTooSwitch = (ImageView) view.findViewById(R.id.ImageToSwitch);
-                    Bundle b = new Bundle();
-                    //b.putParcelable("bitmap", bitmap);
-                    //msg.setData(b);
-                    in.close();
-                }
-                catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-                //messageHandler.sendMessage(msg);
-            }
-        }.start();
-
-        try{
-            Thread.sleep(300);
-        }catch (InterruptedException e){
-            Log.d("ToolsFragment","InterruptedException");
-        }
-
-        moviePoster.setImageBitmap(bitmap);
-        if(bitmap == null) Log.d("ToolsFragment:", "Bitmap == null");
-        Log.d("ToolsFragment", "Bitmapp worked");
-    }
-
-    /*Bitmap getMoviePosterImageFromUrl(String urlString){
-        try {
-            URL url = new URL(urlString);
-            Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-            return bmp;
-        }
-        catch(Exception e){
-            return null;
-        }
-    }*/
-
-
-    private InputStream openHttpConnection(String urlString){
-        InputStream in = null;
-        int resCode = -1;
-
-        try {
-            URL url = new URL(urlString);
-            URLConnection urlConn = url.openConnection();
-            if(!(urlConn instanceof HttpURLConnection)) {
-                throw new IOException("Url is not an Http URL");
-            }
-            HttpURLConnection httpConn = (HttpURLConnection) urlConn;
-            httpConn.setAllowUserInteraction(false);
-            httpConn.setInstanceFollowRedirects(true);
-            resCode = httpConn.getResponseCode();
-
-            if (resCode == HttpURLConnection.HTTP_OK) {
-                in = httpConn.getInputStream();
-            }
-        }
-
-        catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        return in;
     }
 }
--- Moomie2/app/src/main/java/com/example/brianberg/moomie2/ToolsFragment.java
+++ Moomie2/app/src/main/java/com/example/brianberg/moomie2/ToolsFragment.java
@@ -43,49 +43,10 @@ public class ToolsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Switch the image with url
+
         View view = inflater.inflate(R.layout.fragment_tools, container, false);
-        try {
-            TextView text = (TextView) view.findViewById(R.id.SaltText);
-            text.setText("Hello World");
-            ImageView ImageToSwitch = (ImageView) view.findViewById(R.id.ImageToSwitch);
-            Log.d("toolsFragmentStart: ", "ImageView Found");
-            ImageToSwitch.setImageResource(R.drawable.cow);
-            Log.d("toolsFragmentStart: ", "Changed Image");
-        }
-        catch(Exception e){
-            Log.d("Tools Fragment", "NullPointerException");
-        }
 
-        // Get Drawable from URL
-        /*try{
-            Log.d("ToolsFragment", "Getting Movie from URL");
-            InputStream in = openHttpConnection(url);
-
-            /*URL url = new URL("https://s-media-cache-ak0.pinimg.com/736x/66/b4/1c/66b41c8b9f57e17150eba935791ca4ef.jpg");
-            Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-            ImageView ImageTooSwitch = (ImageView) view.findViewById(R.id.ImageToSwitch);
-            ImageTooSwitch.setImageBitmap(bmp);*/
-            /*URL url = new URL("http://ia.media-imdb.com/images/M/MV5BMTk3NDE2NzI4NF5BMl5BanBnXkFtZTgwNzE1MzEyMTE@._V1_SX300.jpg");
-            InputStream is = (InputStream) url.getContent();
-            Log.d("ToolsFragment", "got InputStream");
-
-            Drawable d = Drawable.createFromStream(is,"http://ia.media-imdb.com/images/M/MV5BMTk3NDE2NzI4NF5BMl5BanBnXkFtZTgwNzE1MzEyMTE@._V1_SX300.jpg");
-            Log.d("ToolsFragment", "got drawable");
-
-            ImageView ImageTooSwitch = (ImageView) view.findViewById(R.id.ImageToSwitch);
-            ImageTooSwitch.setImageDrawable(d);
-            Log.d("ToolsFragment", "URL process working");
-        }
-        catch (MalformedURLException e) {
-        e.printStackTrace();
-        } catch (IOException e) {
-            Log.d("ToolsFragment", "Error URL I/O Process");
-            e.printStackTrace();
-        } catch (Exception e) {
-            Log.d("ToolsFragment", "Error URL Process");
-            e.printStackTrace();
-        }*/
+        // Switch the image with url
         new Thread() {
             public void run() {
                 InputStream in = null;
@@ -125,6 +86,7 @@ public class ToolsFragment extends Fragment {
         return view;
     }
 
+    // this code is from some website http://www.tutorialspoint.com/android/android_network_connection.htm
     private InputStream openHttpConnection(String urlString){
         InputStream in = null;
         int resCode = -1;
@@ -144,11 +106,9 @@ public class ToolsFragment extends Fragment {
                 in = httpConn.getInputStream();
             }
         }
-
         catch (MalformedURLException e) {
             e.printStackTrace();
         }
-
         catch (IOException e) {
             e.printStackTrace();
         }
